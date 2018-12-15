b6388b7f36643a0317ad99930b3247a4b4c1ccf2
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-16 00:28:17 +0100

    Updates

---
+++ precache-manifest.f797ccaf7260c25ca290a6c37c7048ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d652ad566864fbf6073a",
+    "url": "/react-news/static/js/runtime~main.d652ad56.js"
+  },
+  {
+    "revision": "fea68364459c2cdce22c",
+    "url": "/react-news/static/js/main.fea68364.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/react-news/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "fea68364459c2cdce22c",
+    "url": "/react-news/static/css/main.468e4690.chunk.css"
+  },
+  {
+    "revision": "596cc75d3adbb7185bf14dc726e55321",
+    "url": "/react-news/index.html"
+  }
+];
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-import App from './App'
-import Drawer from './Drawer'
-
-export { App as default, Drawer }
