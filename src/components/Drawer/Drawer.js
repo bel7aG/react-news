51cd0a2344fd08f21448e7daa21c87933155d912
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-16 16:16:54 +0100

    create my new design for Drawer Component :D

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -7,9 +7,37 @@ export default class Drawer extends Component {
       <div className="drawer">
         <nav className="nav-box">
           <ul className="list-items">
-            <li className="list-item"><NavLink to="/">Home</NavLink></li>
-            <li className="list-item"><NavLink to="/chapters">Chapters</NavLink></li>
-            <li className="list-item"><NavLink to="/contact">Contact</NavLink></li>
+            <li className="list-item">
+              <NavLink to="/">
+                <span>H</span>
+                <span>o</span>
+                <span>m</span>
+                <span>e</span>
+              </NavLink>
+            </li>
+            <li className="list-item">
+              <NavLink to="/chapters">
+                <span>C</span>
+                <span>h</span>
+                <span>a</span>
+                <span>p</span>
+                <span>t</span>
+                <span>e</span>
+                <span>r</span>
+                <span>s</span>
+              </NavLink>
+            </li>
+            <li className="list-item">
+              <NavLink to="/contact">
+                <span>C</span>
+                <span>o</span>
+                <span>n</span>
+                <span>t</span>
+                <span>a</span>
+                <span>c</span>
+                <span>t</span>
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </div>
