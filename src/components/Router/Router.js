fc8da1eaf2938f02035760c4bde47ff33e92b96f
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-17 23:33:48 +0100

    add routers ..

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Main, Drawer, Home, Study, ContactMe, NotFound } from '../'
-import { ReactJS } from '../Study/Components';
+import { ChapterOne, ChapterTwo, ChapterThree, ChapterFoor, ChapterFive, ChapterSix } from '../Study/Components';
 
 const Router = () => (
   <>
@@ -12,12 +12,12 @@ const Router = () => (
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/study" component={Study} />
-            <Route exact path="/study/react-one" component={ReactJS} />
-            <Route exact path="/study/react-two" component={ReactJS} />
-            <Route exact path="/study/react-three" component={ReactJS} />
-            <Route exact path="/study/react-foor" component={ReactJS} />
-            <Route exact path="/study/react-five" component={ReactJS} />
-            <Route exact path="/study/react-six" component={ReactJS} />
+            <Route exact path="/study/react-one" component={ChapterOne} />
+            <Route exact path="/study/react-two" component={ChapterTwo} />
+            <Route exact path="/study/react-three" component={ChapterThree} />
+            <Route exact path="/study/react-foor" component={ChapterFoor} />
+            <Route exact path="/study/react-five" component={ChapterFive} />
+            <Route exact path="/study/react-six" component={ChapterSix} />
             <Route exact path="/contact" component={ContactMe} />
             <Route component={NotFound} />
           </Switch>
