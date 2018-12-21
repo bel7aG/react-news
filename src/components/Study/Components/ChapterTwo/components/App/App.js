1bd9e72f3d6b3064e4daaab0c85ac3e94fc47523
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-21 13:13:47 +0100

    get the geolocation of user .. using api in modern borwsers

--- src/components/Study/Components/ChapterTwo/components/App/App.js
+++ src/components/Study/Components/ChapterTwo/components/App/App.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
-  return (
-    <div><h1>App</h1></div>
-  )
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      latitude: null,
+      isError: ''
+    }
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position)
+        this.setState(() => ({
+          latitude: position.coords.latitude
+        }))
+      },
+      (error) => {
+        this.setState(() => ({
+          isError: `Sorry We can't get yout location sir`
+        }))
+      }
+    )
+
+  }
+
+  render() {
+    return (
+      <div className="chapter-2__location">
+        {this.state.latitude ? `Latitude: ${this.state.latitude}` : `Error: ${this.state.isError}`}
+      </div>
+    )
+  }
 }
-
-export default App
